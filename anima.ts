995572ace53ace0f751dfaa00c4505cc4148606b
Mikulew <mikulew@gmail.com>
2022-02-15 16:18:17 +0100

    Add Anima interface

---
+++ anima.ts
@@ -0,0 +1,18 @@
+interface Anima {
+  soul: '🎇🧚‍♀️',
+  sex: '♀️',
+  personality: [
+    'loving and warm-hearted 💓',
+    'artist 👩‍🎨',
+    'intelligent 👩‍🎓',
+    'funny 🤸‍♀️',
+    'reliant 👩‍❤️‍👨',
+    'welcoming 🙋‍♀️',
+    'competitive 🎱',
+    'sense of rhythm 🎶',
+  ],
+  hobbies: ['🎨', '🎼', '👩‍💻'],
+  favouritedColor: 0xFFFF00,
+  favouritedAnimal: '🐱🐈😺',
+  accessories: ['👒', '🥻', '👠', '👜', '🩳', '💋', '👢'],
+};
